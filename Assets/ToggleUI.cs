60825f86e4eb8094830dca6261c7e651f57a1a34
smmccrty1 <70549146+smmccrty1@users.noreply.github.com>
2020-10-05 16:56:14 -0700

    Toggle UI button

---
+++ Assets/ToggleUI.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ToggleUI : MonoBehaviour
+{
+    public GameObject Canvas;
+    public Boolean B = false;
+
+    public void Toggle()
+    {
+        Canvas.SetActive(B);
+        if (B == false) 
+        { 
+            B = true; 
+        }
+        else { B = false; }
+           
+    }
+}
