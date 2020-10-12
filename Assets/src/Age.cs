65bf451fb6363f52a324880d007303121ed34546
Gavin Gee <galvindgee@gmail.com>
2020-10-12 15:30:05 -0700

    age script final version

--- Assets/src/Age.cs
+++ Assets/src/Age.cs
@@ -1,13 +1,14 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Age : MonoBehaviour
 {
-
-    private int ageInYears;
-    private bool halfYear; 
+    private int ageInYears; 
     private float currentYearPosition;
+    private bool halfYear = false;
+    public Text ageText;
 
     [SerializeField] private GameObject sun;
     private Vector3 sunTransformPosition;
@@ -18,22 +19,41 @@ public class Age : MonoBehaviour
     {
         ageInYears = 0;
         currentYearPosition = 0f;
-        halfYear = false;
         sun = GameObject.Find("SUN");
+        ageText = GameObject.Find("Age Text").GetComponent<Text>();
     }
 
     // Update is called once per frame
-    void FixedUpdate()
+    void Update()
     {
+        // find an angle `theta` between the planet and the x-axis...
         sunTransformPosition = sun.transform.position;
         Vector3 sunToPlanet = this.transform.position - sunTransformPosition;
         float theta = Vector3.Angle(Vector3.right, sunToPlanet);
 
-        if (this.transform.position.y > 0)
+        // find the current "position" in the year (month) using `theta`
+        if (this.transform.position.y > sun.transform.position.y)
         {
-            Debug.Log(theta / 30);
-            return;
+            currentYearPosition = theta / 30;
         }
-        Debug.Log(12 - (theta / 30));
+        else
+        {
+            currentYearPosition = 12 - (theta / 30);
+        }
+
+        // marks the halfway point in the orbit
+        if (Mathf.Abs(currentYearPosition - 6f) < .1)
+        {
+            halfYear = true;
+        }
+        // increments the age in years value by 1 per orbit
+        else if (halfYear && Mathf.Abs(currentYearPosition - 12f) < .01)
+        {
+            ageInYears++;
+            halfYear = false;
+        }
+
+        //Debug.Log("Age: " + ageInYears + " years, " + Mathf.Floor(currentYearPosition) + " months.");
+        ageText.text = "Age: " + ageInYears + " years, " + Mathf.Floor(currentYearPosition) + " months.";
     }
 }
---
+++ Assets/src/AttachAge.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AttachAge : MonoBehaviour
+{ 
+    public Text ageText;
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 pos = Camera.main.WorldToScreenPoint(this.transform.position);
+        pos.y += 2 * this.transform.localScale.y + 20f;
+        ageText.transform.position = pos;
+    }
+}
